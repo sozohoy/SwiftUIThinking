39df478dcc4dd9e4e5eb5cece947b57beb30f08b
sozohoy <sozohoy@gmail.com>
2023-03-14 22:34:23 +0900

    step 15 : Implemeting a ScrollView

---
+++ SwiftUIThinkingBootcamp/ScrollViewBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  ScrollViewBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by 한지석 on 2023/03/14.
+//
+
+import SwiftUI
+
+struct ScrollViewBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScrollViewBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrollViewBootcamp()
+    }
+}
