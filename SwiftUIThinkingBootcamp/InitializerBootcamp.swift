055e111f2e8d1f206502bd31217d231d563c5c18
sozohoy <sozohoy@gmail.com>
2023-03-14 22:04:04 +0900

    step 13 : How to use inits and enums

---
+++ SwiftUIThinkingBootcamp/InitializerBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  InitializerBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by 한지석 on 2023/03/14.
+//
+
+import SwiftUI
+
+struct InitializerBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InitializerBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        InitializerBootcamp()
+    }
+}
