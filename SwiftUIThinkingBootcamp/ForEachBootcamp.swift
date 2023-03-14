fd50a208ced91395956a7f5e8167a5774b60aa0f
sozohoy <sozohoy@gmail.com>
2023-03-14 22:21:14 +0900

    step 14 : How to use ForEach loops

---
+++ SwiftUIThinkingBootcamp/ForEachBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  ForEachBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by 한지석 on 2023/03/14.
+//
+
+import SwiftUI
+
+struct ForEachBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ForEachBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEachBootcamp()
+    }
+}
