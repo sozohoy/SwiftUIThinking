fda7a4ec46a0a486c08bf775f6b02c74d33eb5db
sozohoy <sozohoy@gmail.com>
2023-03-14 23:04:15 +0900

    step 16 : LazyVGrid, LazyHGrid and GridItems

---
+++ SwiftUIThinkingBootcamp/GridBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  GridBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by 한지석 on 2023/03/14.
+//
+
+import SwiftUI
+
+struct GridBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GridBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        GridBootcamp()
+    }
+}
