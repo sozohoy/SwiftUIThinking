8dc6779820e03596b1df00cab6f085db2f7f2ee6
sozohoy <sozohoy@gmail.com>
2023-03-15 13:23:53 +0900

    step 18 : How to add Buttons to SwiftUI

---
+++ SwiftUIThinkingBootcamp/ButtonsBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  ButtonsBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by 한지석 on 2023/03/15.
+//
+
+import SwiftUI
+
+struct ButtonsBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ButtonsBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonsBootcamp()
+    }
+}
