046e9325d61717ef23b7ba964b31a79fbcf84d24
sozohoy <sozohoy@gmail.com>
2023-03-15 13:12:17 +0900

    step 17 : How to use and ignore the Safe Area

---
+++ SwiftUIThinkingBootcamp/SafeAreaBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  SafeAreaBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by 한지석 on 2023/03/15.
+//
+
+import SwiftUI
+
+struct SafeAreaBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SafeAreaBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        SafeAreaBootcamp()
+    }
+}
