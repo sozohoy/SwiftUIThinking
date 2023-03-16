1785b47acb2bf3515897ab2131ab45a907df411d
sozohoy <sozohoy@gmail.com>
2023-03-16 12:45:15 +0900

    step 20 : Extracting functions and subviews

---
+++ SwiftUIThinkingBootcamp/ExtractedFunctionsBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  ExtractedFunctionsBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by 한지석 on 2023/03/16.
+//
+
+import SwiftUI
+
+struct ExtractedFunctionsBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ExtractedFunctionsBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ExtractedFunctionsBootcamp()
+    }
+}
