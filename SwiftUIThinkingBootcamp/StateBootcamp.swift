44606af4858dcac62df78b3a0e7e9bb14d1d200e
sozohoy <sozohoy@gmail.com>
2023-03-16 12:36:42 +0900

    step 19 : How to use State property wrapper

---
+++ SwiftUIThinkingBootcamp/StateBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  StateBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by 한지석 on 2023/03/15.
+//
+
+import SwiftUI
+
+struct StateBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StateBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        StateBootcamp()
+    }
+}
