231cf83a853bee6f24aa8e64acd0ea061ff35a5a
sozohoy <sozohoy@gmail.com>
2023-03-16 13:22:26 +0900

    step 22 : How to use Binding property wrapper

---
+++ SwiftUIThinkingBootcamp/BindingBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  BindingBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by 한지석 on 2023/03/16.
+//
+
+import SwiftUI
+
+struct BindingBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BindingBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        BindingBootcamp()
+    }
+}
