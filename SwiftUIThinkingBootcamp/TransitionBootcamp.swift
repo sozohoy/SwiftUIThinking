23cb1a006b8dc10e49ecd369302b81a9c2620daf
sozohoy <sozohoy@gmail.com>
2023-03-16 20:55:12 +0900

    step 27 : How to use Transition

---
+++ SwiftUIThinkingBootcamp/TransitionBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  TransitionBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by 한지석 on 2023/03/16.
+//
+
+import SwiftUI
+
+struct TransitionBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TransitionBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TransitionBootcamp()
+    }
+}
