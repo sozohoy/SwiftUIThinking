2a9d1ffbe25996870dee770db438b6e32360add6
sozohoy <sozohoy@gmail.com>
2023-03-16 21:22:08 +0900

    step 28 : Display pop-up Sheets and FullScreenCovers

---
+++ SheetsBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  SheetsBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by 한지석 on 2023/03/16.
+//
+
+import SwiftUI
+
+struct SheetsBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SheetsBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        SheetsBootcamp()
+    }
+}
