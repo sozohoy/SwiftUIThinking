8fd08ecbb7a75bec894c321ad85530239d940245
sozohoy <sozohoy@gmail.com>
2023-03-17 20:02:34 +0900

    step 31 : Add, edit, move and delete items

---
+++ SwiftUIThinkingBootcamp/ListBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  ListBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by 한지석 on 2023/03/17.
+//
+
+import SwiftUI
+
+struct ListBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ListBootcamp()
+    }
+}
