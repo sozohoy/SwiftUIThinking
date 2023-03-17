ca6c7b1729ac96de8e8ea4b0646c77f8489ef251
sozohoy <sozohoy@gmail.com>
2023-03-17 19:21:12 +0900

    step 30 : How to use NavigationView and NavigationLink

---
+++ SwiftUIThinkingBootcamp/NavigationViewBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  NavigationViewBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by 한지석 on 2023/03/16.
+//
+
+import SwiftUI
+
+struct NavigationViewBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NavigationViewBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationViewBootcamp()
+    }
+}
