3efba1b98f8a33057a6dbfe0609c818da760e46e
sozohoy <sozohoy@gmail.com>
2023-03-18 17:39:29 +0900

    step 35 : How to use TextField

---
+++ SwiftUIThinkingBootcamp/TextfieldBootCamp.swift
@@ -0,0 +1,20 @@
+//
+//  TextfieldBootCamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by 한지석 on 2023/03/18.
+//
+
+import SwiftUI
+
+struct TextfieldBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TextfieldBootCamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TextfieldBootCamp()
+    }
+}
