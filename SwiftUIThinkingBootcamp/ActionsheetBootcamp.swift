eb35f878dd369e45def1860ba0cdbec91df182f2
sozohoy <sozohoy@gmail.com>
2023-03-18 17:12:15 +0900

    step 33 : How to make a reusable ActionSheet

---
+++ SwiftUIThinkingBootcamp/ActionsheetBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  ActionsheetBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by 한지석 on 2023/03/17.
+//
+
+import SwiftUI
+
+struct ActionsheetBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ActionsheetBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ActionsheetBootcamp()
+    }
+}
