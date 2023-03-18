7ba48dce7a4763cbe51e9a7271a0894d3d976732
sozohoy <sozohoy@gmail.com>
2023-03-18 17:47:53 +0900

    step 36 : How to use TextEditor

---
+++ SwiftUIThinkingBootcamp/TextEditiorBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  TextEditiorBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by 한지석 on 2023/03/18.
+//
+
+import SwiftUI
+
+struct TextEditiorBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TextEditiorBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TextEditiorBootcamp()
+    }
+}
