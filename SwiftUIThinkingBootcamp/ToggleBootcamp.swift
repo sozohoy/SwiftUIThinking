fb769116f50b4eec2f798e6d4385bcf29bc2ea71
sozohoy <sozohoy@gmail.com>
2023-03-18 18:07:18 +0900

    step 37 : How to use a Toggle to create a Switch

---
+++ SwiftUIThinkingBootcamp/ToggleBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  ToggleBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by 한지석 on 2023/03/18.
+//
+
+import SwiftUI
+
+struct ToggleBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ToggleBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ToggleBootcamp()
+    }
+}
