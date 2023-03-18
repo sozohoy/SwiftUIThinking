e8e86f5b300e82dc71f476da89a4d146262fe150
sozohoy <sozohoy@gmail.com>
2023-03-18 18:42:57 +0900

    step 38 : Picker and PickerStyles

---
+++ SwiftUIThinkingBootcamp/PickerBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  PickerBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by 한지석 on 2023/03/18.
+//
+
+import SwiftUI
+
+struct PickerBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PickerBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        PickerBootcamp()
+    }
+}
