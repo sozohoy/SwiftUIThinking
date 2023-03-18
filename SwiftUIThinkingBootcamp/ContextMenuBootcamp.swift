97707d66f69649bd93a5a2f2fa9fe8652facf2d3
sozohoy <sozohoy@gmail.com>
2023-03-18 17:19:43 +0900

    step 34 : How to use ContextMenu

---
+++ SwiftUIThinkingBootcamp/ContextMenuBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  ContextMenuBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by 한지석 on 2023/03/18.
+//
+
+import SwiftUI
+
+struct ContextMenuBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContextMenuBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ContextMenuBootcamp()
+    }
+}
