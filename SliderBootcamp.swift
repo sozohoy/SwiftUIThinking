eadb8e2878510e3d9b948e43f2533c6e54935d6b
sozohoy <sozohoy@gmail.com>
2023-03-20 21:38:30 +0900

    step 42 : How to use Slider

---
+++ SliderBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  SliderBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by 한지석 on 2023/03/20.
+//
+
+import SwiftUI
+
+struct SliderBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SliderBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        SliderBootcamp()
+    }
+}
