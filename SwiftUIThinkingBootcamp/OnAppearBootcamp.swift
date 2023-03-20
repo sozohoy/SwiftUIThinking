273c41ced41d6a09ea6ab997d3956333f628e54d
sozohoy <sozohoy@gmail.com>
2023-03-20 22:48:48 +0900

    step 46 : How to use onAppear and onDisappear

---
+++ SwiftUIThinkingBootcamp/OnAppearBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  OnAppearBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by 한지석 on 2023/03/20.
+//
+
+import SwiftUI
+
+struct OnAppearBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnAppearBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        OnAppearBootcamp()
+    }
+}
