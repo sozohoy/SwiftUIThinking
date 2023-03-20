a39c0bf123249b2248add48aa28d9870f5c31996
sozohoy <sozohoy@gmail.com>
2023-03-20 21:23:37 +0900

    step 39 : How to use ColorPicker

---
+++ ColorPickerBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  ColorPickerBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by 한지석 on 2023/03/20.
+//
+
+import SwiftUI
+
+struct ColorPickerBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ColorPickerBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorPickerBootcamp()
+    }
+}
