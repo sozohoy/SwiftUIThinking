6d05fb0da95374791842c02084310bda5efbcd9c
sozohoy <sozohoy@gmail.com>
2023-03-20 21:58:13 +0900

    step 43 : Create a tab bar with TabView and PageTabViewStyle

---
+++ TabViewBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  TabViewBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by 한지석 on 2023/03/20.
+//
+
+import SwiftUI
+
+struct TabViewBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TabViewBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TabViewBootcamp()
+    }
+}
