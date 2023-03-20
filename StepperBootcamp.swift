11b62fb5a10acc721328a80b2fde29b9ade3f4ce
sozohoy <sozohoy@gmail.com>
2023-03-20 21:32:12 +0900

    step 41 : Create a Stepper

---
+++ StepperBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  StepperBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by 한지석 on 2023/03/20.
+//
+
+import SwiftUI
+
+struct StepperBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StepperBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        StepperBootcamp()
+    }
+}
