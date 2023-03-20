a6512092a85702d05945f994e0226b84c5e37dfe
sozohoy <sozohoy@gmail.com>
2023-03-20 21:27:38 +0900

    step 40 : How to use DatePicker to select dates

---
+++ DatePickerBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  DatePickerBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by 한지석 on 2023/03/20.
+//
+
+import SwiftUI
+
+struct DatePickerBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DatePickerBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        DatePickerBootcamp()
+    }
+}
