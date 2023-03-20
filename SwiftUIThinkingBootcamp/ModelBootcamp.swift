017bbe38a8199c0677adde1aba03cbc11d41b848
sozohoy <sozohoy@gmail.com>
2023-03-20 23:16:22 +0900

    step 49 : How to create custom models

---
+++ SwiftUIThinkingBootcamp/ModelBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  ModelBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by 한지석 on 2023/03/20.
+//
+
+import SwiftUI
+
+struct ModelBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ModelBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ModelBootcamp()
+    }
+}
