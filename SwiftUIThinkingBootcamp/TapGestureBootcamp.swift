c7806e27a46f6533e8f13818063b71e6bb490156
sozohoy <sozohoy@gmail.com>
2023-03-20 23:02:31 +0900

    step 48 : How to add a Tap Gesture

---
+++ SwiftUIThinkingBootcamp/TapGestureBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  TapGestureBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by 한지석 on 2023/03/20.
+//
+
+import SwiftUI
+
+struct TapGestureBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TapGestureBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TapGestureBootcamp()
+    }
+}
