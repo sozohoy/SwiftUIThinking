60b4a8143376a8f7d8847064f76d68a1cee28773
sozohoy <sozohoy@gmail.com>
2023-03-23 20:27:31 +0900

    step 50 : How to use @ObservableObject and @StateObject

---
+++ SwiftUIThinkingBootcamp/ViewModelBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  ViewModelBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by 한지석 on 2023/03/20.
+//
+
+import SwiftUI
+
+struct ViewModelBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ViewModelBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewModelBootcamp()
+    }
+}
